dcc93138af96b462473f47487c6fbf88bc53294c
Matt Flax <flatmax@flatmax.org>
2020-11-27 09:10:55 +1100

    startSound.sh updates.

--- overlays/root/startSound.sh
+++ overlays/root/startSound.sh
@@ -8,6 +8,7 @@ AI_PATH=$BR_PATH/output/build/audioinjector-alsa-0.0.1/audioinjector-nanopi
 HOST_SUNXI_PATH=/lib/modules/5.3.13/kernel/sound/soc/sunxi
 scp $AI_PATH/snd-soc-audioinjector-nanopi-neo-stereo.ko $SUNXI_PATH/sun4i-i2s.ko  root@audioinjector.local:$HOST_SUNXI_PATH
 
+ssh root@$HOST "killall arecord; killall aplay; sleep 5"
 ssh root@$HOST "rmmod snd-soc-audioinjector-nanopi-neo-stereo snd-soc-wm8731 sun4i-i2s snd_soc_core snd_pcm_dmaengine snd_pcm snd_timer snd"
 ssh root@$HOST "/etc/init.d/S30start-audio start"
 ssh root@$HOST "alsactl restore"
