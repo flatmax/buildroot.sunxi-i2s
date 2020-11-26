8a3e02b78226116902094e4e97fc5fff10cbce7f
Matt Flax <flatmax@flatmax.org>
2020-11-27 06:51:45 +1100

    update modules and startSound.sh

--- overlays/root/startSound.sh
+++ overlays/root/startSound.sh
@@ -1,24 +1,13 @@
 #!/usr/bin/env bash
-if [ $# -eq "0" ]; then
-  HOST=192.168.1.4
-else
-  HOST=$1
-fi
-ssh-keygen -f ~/.ssh/known_hosts -R $HOST
+
+HOST=audioinjector.local
 sleep_time=0
-BUILD=/home/flatmax.unencrypted/buildroot.neo4/output/build
-scp $BUILD/audioinjector-alsa-0.0.1/audioinjector-nanopi/*.ko root@$HOST:/lib64/modules/4.4.143/kernel/sound/soc/rockchip
-scp $BUILD/akm-codecs-0.0.1/codecs/*.ko root@$HOST:/lib64/modules/4.4.143/kernel/sound/soc/codecs
-ssh root@$HOST "echo 1; modprobe -r snd_soc_audioinjector_nanopi_akm; sleep $sleep_time"
-ssh root@$HOST "echo 2; rmmod snd-soc-ak4137; sleep $sleep_time"
-ssh root@$HOST "echo 3; rmmod snd-soc-ak5572; sleep $sleep_time"
-ssh root@$HOST "echo 4; rmmod snd-soc-ak4490; sleep $sleep_time"
-ssh root@$HOST "echo 5; modprobe snd-soc-ak4137; sleep $sleep_time"
-ssh root@$HOST "echo 6; modprobe snd-soc-ak5572; sleep $sleep_time"
-ssh root@$HOST "echo 7; modprobe snd-soc-ak4490; sleep $sleep_time"
-ssh root@$HOST "echo 8; modprobe snd_soc_audioinjector_nanopi_akm"
-# sleep 1
-# ssh root@$HOST "aplay test.wav"
-# # echo aplay test.wav
-# echo aplay -B 100000 -Dhw:0,2 tone.wav
-# #echo aplay -c 2 -f S32_LE -r 48000 /dev/random
+BR_PATH=/home/flatmax.unencrypted/buildroot.NanoPi.Neo
+SUNXI_PATH=$BR_PATH/output/build/linux-5.3.13/sound/soc/sunxi
+AI_PATH=$BR_PATH/output/build/audioinjector-alsa-0.0.1/audioinjector-nanopi
+HOST_SUNXI_PATH=/lib/modules/5.3.13/kernel/sound/soc/sunxi
+scp $AI_PATH/snd-soc-audioinjector-nanopi-neo-stereo.ko $SUNXI_PATH/sun4i-i2s.ko  root@audioinjector.local:$HOST_SUNXI_PATH
+
+ssh root@$HOST "rmmod snd-soc-audioinjector-nanopi-neo-stereo snd-soc-wm8731 sun4i-i2s snd_soc_core snd_pcm_dmaengine snd_pcm snd_timer snd"
+ssh root@$HOST "/etc/init.d/S30start-audio start"
+ssh root@$HOST "alsactl restore"
